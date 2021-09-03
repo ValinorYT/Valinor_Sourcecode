9be981a11bc595e559c96bf1568b2ecfa499b5df
MoritzGross <mgross321@gmail.com>
2021-09-03 21:40:24 +0200

    alködjblköfdg

--- src/utils/distances.py
+++ src/utils/distances.py
@@ -4,3 +4,22 @@ import numpy as np
 def nearest_pos(x, nb):
     distances = np.linalg.norm(np.array(x) - np.array(nb), axis=1)
     return nb[distances.argmin()]
+
+
+# Not stable yet
+def positions_sorted_by_distance(x, nb):
+    distances = np.linalg.norm(np.array(x) - np.array(nb), axis=1)
+    sorted_indices = distances.argsort()
+    return np.array(nb)[sorted_indices]
+
+
+# Used for quick testing right now
+x1 = [0, 0, 0]
+x2 = [2, 2, 3]
+x3 = [200, 10 ** 4, -4567]
+nb1 = [[1, 1, 1], [2, 2, 2]]
+nb2 = [[0, 100, 0]]
+
+if __name__ == '__main__':
+    x = positions_sorted_by_distance(x1, nb1)
+    print(x)
--- test/test_distances.py
+++ test/test_distances.py
@@ -1,20 +1,21 @@
 from unittest import TestCase
 
-from src.utils.distances import nearest_pos
+from src.utils.distances import nearest_pos, positions_sorted_by_distance
+
+x1 = [0, 0, 0]
+x2 = [2, 2, 3]
+x3 = [200, 10 ** 4, -4567]
+nb1 = [[1, 1, 1], [2, 2, 2]]
+nb2 = [[0, 100, 0]]
 
 
 class Test(TestCase):
-    x1 = [0, 0, 0]
-    x2 = [2, 2, 3]
-    x3 = [200, 10 ** 4, -4567]
-    nb1 = [[1, 1, 1], [2, 2, 2]]
-    nb2 = [[0, 100, 0]]
 
     def test_nearest_point1(self):
-        self.assertEqual(self.nb1[0], nearest_pos(self.x1, self.nb1))
+        self.assertEqual(nb1[0], nearest_pos(x1, nb1))
 
     def test_nearest_point2(self):
-        self.assertEqual(self.nb1[1], nearest_pos(self.x2, self.nb1))
+        self.assertEqual(nb1[1], nearest_pos(x2, nb1))
 
     def test_nearest_point3(self):
-        self.assertEqual(self.nb2[0], nearest_pos(self.x3, self.nb2))
+        self.assertEqual(nb2[0], nearest_pos(x3, nb2))
