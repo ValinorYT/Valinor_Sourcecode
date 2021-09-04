c23ee4626d489cb40e8279749ce1b42ec5ba891f
MoritzGross <mgross321@gmail.com>
2021-09-04 19:31:14 +0200

    cleaned up HashWrapper.py

--- src/utils/HashWrapper.py
+++ src/utils/HashWrapper.py
@@ -2,11 +2,11 @@ class HashWrapper:
     def __init__(self, val):
         self.val = val
 
-    def __hash__(self):
-        return hash(str(self.val))
-
     def __repr__(self):
         return str(self.val)
 
+    def __hash__(self):
+        return hash(self.__repr__())
+
     def __eq__(self, other):
-        return str(self.val) == str(other.val)
+        return self.__repr__() == other.__repr__()
