8d6aa9a139fbeb861246f5d314101ac9e79c9703
MoritzGross <mgross321@gmail.com>
2021-09-04 19:31:00 +0200

    removed depreciated method

--- src/utils/distances.py
+++ src/utils/distances.py
@@ -1,26 +1,6 @@
 import numpy as np
-from manim import Dot
 
 
-def nearest_pos(x, nb):
-    distances = np.linalg.norm(np.array(x) - np.array(nb), axis=1)
-    return nb[distances.argmin()]
-
-
-# Not stable yet
-def dots_sorted_by_distance(x, nb):
+def dots_sorted_by_distance(x, my_dots):
     x_np = np.array(x.get_center())
-    return sorted(nb, key=lambda i: np.linalg.norm(x_np - np.array(i.get_center())))
-
-
-# Used for quick testing right now
-x1 = Dot([0, 0, 0])
-x2 = Dot([2, 2, 3])
-x3 = Dot([200, 10 ** 4, -4567])
-nb1 = [Dot([1, 1, 1]), Dot([2, 2, 2])]
-nb2 = [Dot([0, 100, 0])]
-
-if __name__ == '__main__':
-    x = dots_sorted_by_distance(x1, nb1)
-    print(x[0].get_center())
-    print(x[1].get_center())
+    return sorted(my_dots, key=lambda i: np.linalg.norm(x_np - np.array(i.get_center())))
